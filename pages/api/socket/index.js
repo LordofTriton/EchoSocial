a488c350700abc95aeace3570bd0ac80a2743b8f
LordofTriton <agboola918@gmail.com>
2023-08-29 00:38:20 +0100

    Fixed socket issue.

--- pages/api/socket/index.js
+++ pages/api/socket/index.js
@@ -14,7 +14,6 @@ import MemberSocket from '../community-members/socket';
 
 export default async (request, response) => {
     if (!response.socket.server.io) {
-    const { db } = await getDB();
         console.log("Socket: Initialized.")
         const io = new Server(response.socket.server, { path: "/api/socket", addTrailingSlash: false, cors: {
                 origin: "/",
