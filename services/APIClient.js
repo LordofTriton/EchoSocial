8eef7c41b384af802fb14fd678a7f4cd429555d0
LordofTriton <agboola918@gmail.com>
2023-08-28 21:07:54 +0100

    Fixed compile issues.

--- services/APIClient.js
+++ services/APIClient.js
@@ -2,21 +2,21 @@ import axios from "axios"
 import AppConfig from "../util/config"
 
 function post(url, data, headers) {
-    return axios.post(`${url}`, data, {
+    return axios.post(`/api${url}`, data, {
         headers
     })
 }
 
 function put(url, data, headers) {
-    return axios.put(`${url}`, data)
+    return axios.put(`/api${url}`, data)
 }
 
 function get(url, headers) {
-    return axios.get(`${url}`)
+    return axios.get(`/api${url}`)
 }
 
 function del(url, headers) {
-    return axios.delete(`${url}`)
+    return axios.delete(`/api${url}`)
 }
 
 const APIClient = { post, put, get, del }
