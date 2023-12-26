4ff41db55ecb9f3388230c4ee2b54038aeded7a3
LordofTriton <agboola918@gmail.com>
2023-12-27 00:44:58 +0100

    Added user nickname to echo.

--- services/PusherClient.js
+++ services/PusherClient.js
@@ -1,7 +1,7 @@
 import pusherJs from "pusher-js";
 
-const PusherClient = new pusherJs("50f5658f71430c02353d", {
-    cluster: "eu"
+const PusherClient = new pusherJs(process.env.PUSHER_KEY, {
+    cluster: process.env.PUSHER_CLUSTER
 });
 
 export default PusherClient;
--- services/PusherServer.js
+++ services/PusherServer.js
@@ -1,15 +1,10 @@
 const Pusher = require("pusher");
 
-const appId = "1727471";
-const key = "50f5658f71430c02353d";
-const secret = "7b8da5c92f2012db801a";
-const cluster = "eu";
-
 const PusherServer = new Pusher({
-    appId,
-    key,
-    secret,
-    cluster,
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: process.env.PUSHER_CLUSTER,
     useTLS: true
 });
 
